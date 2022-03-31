7b6f9284f3478ca67479ce1450111560aeef5efb
Manuela <manuelagarcialira@gmail.com>
2022-03-30 21:49:58 -0300

    structure project

--- Lab01/Lab01.X/main.c
+++ Lab01/Lab01.X/main.c
@@ -1,4 +1,3 @@
-
 // PIC32MM0256GPM064 Configuration Bit Settings
 
 // 'C' source line config statements
@@ -43,157 +42,49 @@
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
-#include <xc.h>
 
-/* ************************************************************************** */
-/** Descriptive File Name
 
-  @Company
-    Company Name
-
-  @File Name
-    filename.c
-
-  @Summary
-    Brief description of the file.
-
-  @Description
-    Describe the purpose of this file.
- */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
 /* ************************************************************************** */
 /* Section: Included Files                                                    */
 /* ************************************************************************** */
-/* ************************************************************************** */
 
-/* This section lists the other files that are included in this file.
- */
+#include <xc.h>
+#include <stdio.h>
 
-/* TODO:  Include other files here if needed. */
-
-
-/* ************************************************************************** */
+#define ARRAY_SIZE 12
 /* ************************************************************************** */
 /* Section: File Scope or Global Data                                         */
 /* ************************************************************************** */
-/* ************************************************************************** */
 
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
-
-/* ************************************************************************** */
-/** Descriptive Data Item Name
-
-  @Summary
-    Brief one-line summary of the data item.
-    
-  @Description
-    Full description, explaining the purpose and usage of data item.
-    <p>
-    Additional description in consecutive paragraphs separated by HTML 
-    paragraph breaks, as necessary.
-    <p>
-    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-    
-  @Remarks
-    Any additional remarks
- */
 int global_data;
+int exampleData;
+char exampleArray[ARRAY_SIZE];
 
-
-/* ************************************************************************** */
 /* ************************************************************************** */
 // Section: Local Functions                                                   */
 /* ************************************************************************** */
-/* ************************************************************************** */
 
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
 
 /* ************************************************************************** */
 
-/** 
-  @Function
-    int ExampleLocalFunctionName ( int param1, int param2 ) 
 
-  @Summary
-    Brief one-line description of the function.
-
-  @Description
-    Full description, explaining the purpose and usage of the function.
-    <p>
-    Additional description in consecutive paragraphs separated by HTML 
-    paragraph breaks, as necessary.
-    <p>
-    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-
-  @Precondition
-    List and describe any required preconditions. If there are no preconditions,
-    enter "None."
-
-  @Parameters
-    @param param1 Describe the first parameter to the function.
-    
-    @param param2 Describe the second parameter to the function.
-
-  @Returns
-    List (if feasible) and describe the return values of the function.
-    <ul>
-      <li>1   Indicates an error occurred
-      <li>0   Indicates an error did not occur
-    </ul>
-
-  @Remarks
-    Describe any special behavior not described above.
-    <p>
-    Any additional remarks.
-
-  @Example
-    @code
-    if(ExampleFunctionName(1, 2) == 0)
-    {
-        return 3;
-    }
- */
 static int ExampleLocalFunction(int param1, int param2) {
     return 0;
 }
 
 
 /* ************************************************************************** */
-/* ************************************************************************** */
 // Section: Interface Functions                                               */
 /* ************************************************************************** */
-/* ************************************************************************** */
 
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
 
-// *****************************************************************************
-
-/** 
-  @Function
-    int ExampleInterfaceFunctionName ( int param1, int param2 ) 
-
-  @Summary
-    Brief one-line description of the function.
-
-  @Remarks
-    Refer to the example_file.h interface header for function usage details.
- */
 int main( void ){
+   
     while(1){
-        Nop();
+        Nop();              
     }
     return 0;
 }
 
 
-/* *****************************************************************************
- End of File
- */
+/* ***************************************************************************** End of File  */
---
+++ Lab01/Lab01.X/platform/HardwareProfile.h
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+    Company Name
+
+  @File Name
+    filename.h
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
+
+#ifndef _HARDWARE_PROFILE_H    /* Guard against multiple inclusion */
+#define _HARDWARE_PROFILE_H
+#define LEDA_SetDigitalOutput() (TRISAbits.TRISA7 = 0)
+#define LEDB_SetDigitalOutput() (TRISBbits.TRISB14 = 0)
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Included Files                                                    */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/* This section lists the other files that are included in this file.
+ */
+
+/* TODO:  Include other files here if needed. */
+
+
+/* Provide C++ Compatibility */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    /* ************************************************************************** */
+    /* ************************************************************************** */
+    /* Section: Constants                                                         */
+    /* ************************************************************************** */
+    /* ************************************************************************** */
+
+#define EXAMPLE_CONSTANT 0
+#define LEDX_SetHigh() // Pone en nivel alto
+#define LEDX_SetLow() // Pone en nivel bajo
+#define LEDX_Toggle() // Invierte de nivel
+
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: Data Types
+    // *****************************************************************************
+    // *****************************************************************************
+
+    /*  A brief description of a section can be given directly below the section
+        banner.
+     */
+
+
+    // *****************************************************************************
+
+    typedef struct _example_struct_t {
+        /* Describe structure member. */
+        int some_number;
+
+        /* Describe structure member. */
+        bool some_flag;
+
+    } example_struct_t;
+
+
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: Interface Functions
+    // *****************************************************************************
+    // *****************************************************************************
+
+    /*  A brief description of a section can be given directly below the section
+        banner.
+     */
+
+    // *****************************************************************************
+   
+    int ExampleFunction(int param1, int param2);
+
+
+    /* Provide C++ Compatibility */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _EXAMPLE_FILE_NAME_H */
+
+/* *****************************************************************************
+ End of File
+ */
